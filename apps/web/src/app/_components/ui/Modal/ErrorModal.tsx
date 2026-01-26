9257f437ed1a4d1142ddfede461ff578e1119e69
gmlwlsdl <heejin094@naver.com>
2026-01-26 22:32:32 +0900

    [Design] 에러 대응 페이지 및 모달 디자인 수정 (#125)

--- apps/web/src/app/_components/ui/Modal/ErrorModal.tsx
+++ apps/web/src/app/_components/ui/Modal/ErrorModal.tsx
@@ -14,20 +14,12 @@ const ErrorModal = ({ isOpen, title, message, onClose }: ErrorModalProps) => {
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col items-center gap-4">
-        <Image
-          src="/images/momuzzi-error.svg"
-          alt="모무찌 에러 아이콘"
-          width={80}
-          height={0}
-          style={{ width: 'auto', height: 'auto' }}
-        />
-
         <div className="mb-2 flex flex-col gap-1">
           <h3 className="text-center body-2 font-semibold">{title}</h3>
           <p className="text-center body-3 whitespace-pre-wrap text-neutral-800">{message}</p>
         </div>
 
-        <Button className="w-full" onClick={onClose}>
+        <Button className="h-13 w-full" onClick={onClose} theme="orange">
           확인
         </Button>
       </div>
--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -6,24 +6,21 @@ import Button from '@/app/_components/ui/Button';
 const NotFoundPage = () => {
   return (
     <main className="flex h-[100dvh] flex-col select-none">
-      <div className="flex flex-1 flex-col items-center justify-center gap-5">
-        <Image
-          src="/images/momuzzi-not-found.svg"
-          alt="모무찌 404 아이콘"
-          width={180}
-          height={120}
-        />
-        <div className="flex flex-col gap-1 text-center">
+      <div className="flex flex-1 flex-col items-center justify-center gap-5 px-5">
+        <div className="flex w-full flex-col gap-1 text-center">
           <h3 className="body-1 font-semibold text-neutral-1600">
-            찾으시는 페이지를 찾을 수 없어요.
+            원하시는 페이지를 찾을 수 없어요.
           </h3>
-          <p className="body-3 whitespace-pre text-neutral-800">
-            {`페이지 주소가 바뀌었거나, 잠시 접근이 어려운 상태예요.\n홈으로 돌아가 다시 시작해보세요.`}
+          <p className="body-3 font-medium whitespace-pre text-neutral-800">
+            {`찾으시는 페이지의 주소 혹은 이름이 변경되었거나,\n일시적으로 사용할 수 없는 상태에요.`}
+          </p>
+          <p className="body-3 font-medium whitespace-pre text-neutral-800">
+            {`문제가 또 발생한다면,\nssolvofficial@gmail.com로 알려주세요.`}
           </p>
         </div>
       </div>
       <Link href="/" className="px-5 pt-3 pb-6">
-        <Button>홈으로 가기</Button>
+        <Button theme="orange">홈으로 가기</Button>
       </Link>
     </main>
   );
