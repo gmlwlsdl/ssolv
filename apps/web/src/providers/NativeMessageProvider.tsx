f0b8860aa6b69dfabe62cb2fca11a654a2b26f9e
gmlwlsdl <heejin094@naver.com>
2026-02-28 12:30:33 +0900

    [Feat] WebView nativeMessage 이벤트 처리 훅 및 프로바이더 추가 (#130)

---
+++ apps/web/src/providers/NativeMessageProvider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import useNativeMessage from '@/hooks/useNativeMessage';
+
+/**
+ * 모바일 WebView nativeMessage 이벤트 리스너를 앱 전역에 등록하는 프로바이더
+ */
+const NativeMessageProvider = ({ children }: { children: React.ReactNode }) => {
+  useNativeMessage();
+  return <>{children}</>;
+};
+
+export default NativeMessageProvider;
